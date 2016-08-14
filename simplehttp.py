c304c801ea84d357b85486edef70c3f8f11b61ac
priitp <priit.piipuu@example.com>
2016-08-14 22:40:38 +0300

    Provide implementation.

--- simplehttp.py
+++ simplehttp.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
 
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 import SimpleHTTPServer
 import SocketServer
 import logging
 
 class EchoHandler(BaseHTTPRequestHandler):
-
+    def do_GET(self):
+        logging.info("Handling GET:")
+        logging.info("request path = '{}'".format(self.path))
+        logging.info("headers = '{}'".format(self.headers))
+        logging.info("GET done.")
+        self.send_response(200)
+        self.send_cookie("Set-Cookie", "hello=simplehttp")
+        self.end_headers()
+    def do_POST(self):
+        logging.info("Handling POST:")
+        logging.info("request path = '{}'".format(self.path))
+        logging.info("headers = '{}'".format(self.headers))
+        logging.info("GET done.")
+        self.send_response(200)
+        self.end_headers()
+    
+if __name__ == "__main__":
+    port = 8080
+    server = HTTPServer(('', port), EchoHandler)
+    server.serve_forever()
