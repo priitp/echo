a61ccd6b51d7634c83a9ed6ef20a3df7a3abfb7c
Priit Piipuu <priit.piipuu@gmail.com>
2016-08-14 16:13:38 +0300

    Add somewhat more elaborate logging setup.

--- simplehttp.py
+++ simplehttp.py
@@ -6,19 +6,26 @@ import SocketServer
 import logging
 
 class EchoHandler(BaseHTTPRequestHandler):
+    logger = logging.getLogger('EchoHandler')
+    logger.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
     def do_GET(self):
-        logging.warning("Handling GET:")
-        logging.warning("request path = '{}'".format(self.path))
-        logging.warning("headers = {}".format(self.headers))
-        logging.warning("GET done.")
+        self.logger.info("Handling GET:")
+        self.logger.info("request path = '{}'".format(self.path))
+        self.logger.info("headers = {}".format(self.headers))
+        self.logger.info("GET done.")
         self.send_response(200)
         self.send_header("Set-Cookie", "hello=simplehttp")
         self.end_headers()
     def do_POST(self):
-        logging.warning("Handling POST:")
-        logging.warning("request path = '{}'".format(self.path))
-        logging.warning("headers = '{}'".format(self.headers))
-        logging.warning("GET done.")
+        self.logger.info("Handling POST:")
+        self.logger.info("request path = '{}'".format(self.path))
+        self.logger.info("headers = '{}'".format(self.headers))
+        self.logger.info("GET done.")
         self.send_response(200)
         self.end_headers()
     
